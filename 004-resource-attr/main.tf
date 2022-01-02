5ad25e53e4f1034c1588ec8de96eaf37503736ea
keys4words <keys4words@gmail.com>
2022-01-02 17:58:23 +0300

    add timestamp pipe

--- 004-resource-attr/main.tf
+++ 004-resource-attr/main.tf
@@ -8,3 +8,11 @@ resource "random_pet" "my-pet" {
   separator = var.separator
   length = var.length
 }
+
+resource "time_static" "time_update" {
+}
+
+resource "local_file" "time" {
+  filename = "time.txt"
+  content = "Time stamp of this file is ${time_static.time_update.id}"
+}
